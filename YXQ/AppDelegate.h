540fc6b2d42373ccce77056c0007c1bf70160a5e
apple <yeyexy@yuanye.local>
2016-09-22 09:55:48 +0800

    Initial Commit

---
+++ YXQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXQ
+//
+//  Created by 袁野 on 16/9/22.
+//  Copyright © 2016年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXQ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXQ
+//
+//  Created by 袁野 on 16/9/22.
+//  Copyright © 2016年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
